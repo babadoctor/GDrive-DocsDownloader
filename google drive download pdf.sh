41b35a5fb3fbdda7c48accc76835adb44f805e65
babadoctor <nobletabibian@gmail.com>
2018-08-24 09:18:11 -0700

    Add files via upload

---
+++ google drive download pdf.sh
@@ -0,0 +1,6 @@
+for i in `seq 0 85`;
+do
+wget -O $i.png "https://drive.google.com/viewerng/img?id=ACFrOgDmbzfuFijZaBu9NJJWZdcuKU_dLOBrdiGv4aZ9TmM0emgQ7rKvNgfGQzhK6bRBZF2hIEMc45p4cKy2vjQV43c7Y4T8tuuCJw-xyLuVhPYjvSa60jJwoErNgU8%3D&page=$i&skiphighlight=true&w=1600&webp=false"
+done
+
+
